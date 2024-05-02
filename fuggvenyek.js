76f5aa0b6703873cbdc8c9727f7dd976f0d342e7
Logan114 <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>
2024-05-02 09:29:34 +0200

    Kosar gomb hozzaadasa

--- fuggvenyek.js
+++ fuggvenyek.js
@@ -1,25 +1,30 @@
-
-export function kereses(lista){
-    const kereso = $(".in")
-        kereso.on("keyup",function(){
-            console.log("🦀")
-            let keresett = kereso.val()
-            const SZURTLiSTA = lista.filter(function (lista) {
-                let talalt = lista.nev.includes(keresett);
-            });
-            
-                
-            })
-        }
-    
+export function kereses(lista) {
+  const kereso = $(".in");
+  kereso.on("keyup", function () {
+    console.log("🦀");
+    let keresett = kereso.val();
+    const SZURTLiSTA = lista.filter(function (lista) {
+      let talalt = lista.nev.includes(keresett);
+    });
+  });
+}
 
 export function szuresNevSzerint(lista, szurtSzoveg) {
-        /* szurtLista - a szűrőbe írt szó alaján kilistázza azokat az adatokat a listából, amelyekben szerepel a név mezőjében az adott szó.  */
-        const SZURTLiSTA = lista.filter(function (elem) {
-          return elem.nev.includes(szurtSzoveg);
-        });
-        console.log(SZURTLiSTA);
-        return SZURTLiSTA;
-      }
+  /* szurtLista - a szűrőbe írt szó alaján kilistázza azokat az adatokat a listából, amelyekben szerepel a név mezőjében az adott szó.  */
+  const SZURTLiSTA = lista.filter(function (elem) {
+    return elem.nev.includes(szurtSzoveg);
+  });
+  console.log(SZURTLiSTA);
+  return SZURTLiSTA;
+}
 
-
+export function szortirozasArSzerint(lista) {
+  lista.sort(function (a, b) {
+    if (a.ar < b.ar) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+  console.log(lista)
+}
--- main.js
+++ main.js
@@ -1,42 +1,70 @@
-import { termekLista } from "./adatlista.js"
-import { szuresNevSzerint } from "./fuggvenyek.js"
-init(termekLista)
+import { termekLista } from "./adatlista.js";
+import { szuresNevSzerint, szortirozasArSzerint } from "./fuggvenyek.js";
+init(termekLista);
+let kosarLista = [ ]
 
 function init(lista) {
   let txt = weboldal_letrehozas(lista);
-
+  const arSzerintHTML = $(".crab");
+  weboldal_megjelenites(txt);
+  szuresKattintasra(termekLista, arSzerintHTML);
+  console.log("🦀");
 }
 
-function weboldal_letrehozas(lista){
-    const termekekHTML = $(".termekek")
-    let i=0
-    let txt =""
-    lista.forEach((element,id) => {
-        txt +=(`<div class="card termek${id}">`)
-        txt +=(`<div class="card">`)
-        txt +=(`<img src="${lista[id].img}" alt="polo" />`)
-        txt +=(`<h1>${lista[id].nev} </h1>`)
-        txt +=(`<h3 class=ar${id}>${lista[id].ar} </h3>`)
-        txt +=(`<div class="btn btn-primary btn-block termek${id}">
-        <p>kosárba</p>
-      </div>`)
-      
-        txt +=(`</div>`)
-        txt +=(`</div>`)
-    });
-    termekekHTML.html(txt)
-    
+function weboldal_megjelenites(txt) {
+  const termekekHTML = $(".termekek");
+  termekekHTML.html(txt);
 }
+function weboldal_letrehozas(lista) {
+  let i = 0;
+  let txt = "";
+  termekLista.forEach((element, id) => {
+    txt += `<div class="card termek${id}">`;
+    txt += `<div class="card">`;
+    txt += `<img src="${termekLista[id].img}" alt="polo" />`;
+    txt += `<h1>${termekLista[id].nev} </h1>`;
+    txt += `<h3 class=ar${id}>${termekLista[id].ar} </h3>`;
+    txt += `<button type="button" class="btn btn-primary btn-block termek${id} kosar" onclick="kosarLista.push(${id})"> kosarba</button>
+    </div>`;
 
+    txt += `</div>`;
+    txt += `</div>`;
+  });
+  return txt;
+}
+function kosargomb(gomb){
+  const gombHTML =$(".kosar")
+  console.log(kosarLista)
+}
 
 function nevSzuresEsemeny(lista) {
   const szuroELEM = $(".in");
   szuroELEM.on("keyup", function () {
     let szuroSZoveg = szuroELEM.val();
     const LISTA = szuresNevSzerint(lista, szuroSZoveg);
-    console.log(LISTA)
-    init(LISTA)
+    console.log(LISTA);
+    let txt = "";
+    termekLista.forEach((element, id) => {
+      txt += `<div class="card termek${id}">`;
+      txt += `<div class="card">`;
+      txt += `<img src="${termekLista[id].img}" alt="polo" />`;
+      txt += `<h1>${termekLista[id].nev} </h1>`;
+      txt += `<h3 class=ar${id}>${termekLista[id].ar} </h3>`;
+      txt += `<div class="btn btn-primary btn-block termek${id}">
+    <p>kosárba</p>
+    </div>`;
+
+      txt += `</div>`;
+      txt += `</div>`;
+    });
+    weboldal_megjelenites(txt)
   });
 }
-nevSzuresEsemeny(termekLista)
+nevSzuresEsemeny(termekLista);
 
+function szuresKattintasra(lista, htmlElem) {
+  htmlElem.on("click", function () {
+    const LISTA = szortirozasArSzerint(lista);
+    init(LISTA);
+  });
+}
